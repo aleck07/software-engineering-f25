4f457947b8f1d7fd766a610d1efa15d5bd37d523
Alec <aleckrsek@gmail.com>
2025-10-22 10:48:08 -0700

    Finish in class 4-2-1

---
+++ week4/inclass4-2-1.rb
@@ -0,0 +1,28 @@
+class File
+  def initialize(file_name)
+    @file_name = file_name
+    @file_pointer
+  end
+
+  def open
+    @file_pointer = open_file(@file_name)
+  end
+  
+  def read
+    read_data(@file_pointer)
+  end
+
+  def write(data)
+    write_data(@file_pointer, data)
+  end
+
+  def close()
+    close_file(@file_pointer)
+  end
+end
+
+file = File.new("example.txt")
+file.open
+file.write("Hello!")
+puts file.read
+file.close
