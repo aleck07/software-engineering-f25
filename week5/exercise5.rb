776edfc41b3471e34a97ceae77d51f750bf00f71
Alec <aleckrsek@gmail.com>
2025-11-06 09:24:15 -0800

    Finish exercise 5

--- week5/exercise5.rb
+++ week5/exercise5.rb
@@ -6,37 +6,40 @@ class LaunchDiscussionWorkflow
     @discussion = discussion
     @host = host
     @participants_email_string = participants_email_string
-    @participants = []
   end
 
-  # Expects @participants array to be filled with User objects
   def run
-    return unless valid?
-    run_callbacks(:create) do
-      ActiveRecord::Base.transaction do
-        discussion.save!
-        create_discussion_roles!
-        @successful = true
-      end
+    return false unless valid?
+    generate_participants
+    ActiveRecord::Base.transaction do
+      @discussion.save!
+      create_discussion_roles
     end
+    true
   end
 
-  def generate_participant_users_from_email_string
+  private def valid?
+    @discussion.present? && @host.present?
+  end
+
+  def generate_participants
     return if @participants_email_string.blank?
-    @participants_email_string.split.uniq.map do |email_address|
+    @participants = @participants_email_string.split.uniq.map do |email_address|
       User.create(email: email_address.downcase, password: Devise.friendly_token)
     end
   end
 
-  # ...
-
+  def create_discussion_roles
+    @participants.each do |participant|
+      DiscussionRole.create(discussion: @discussion, user: participant)
+    end
+  end
 end
 
 
-discussion = Discussion.new(title: "fake", ...)
+discussion = Discussion.new(title: "fake")
 host = User.find(42)
 participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
 
 workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
-workflow.generate_participant_users_from_email_string
 workflow.run
